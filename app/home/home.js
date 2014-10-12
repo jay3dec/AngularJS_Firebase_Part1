3acadee4e189550e7ac36e55c3a616515c4a1eca
jay <jay3dec@gmail.com>
2014-10-12 21:00:06 +0530

    first

---
+++ app/home/home.js
@@ -0,0 +1,33 @@
+'use strict';
+
+angular.module('myApp.home', ['ngRoute','firebase'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/home', {
+    templateUrl: 'home/home.html',
+    controller: 'HomeCtrl'
+  });
+}])
+
+.controller('HomeCtrl', ['$scope','$firebaseSimpleLogin',function($scope,$firebaseSimpleLogin) {
+  var firebaseObj = new Firebase("https://blistering-heat-2473.firebaseio.com");
+var loginObj = $firebaseSimpleLogin(firebaseObj);
+  
+  $scope.user = {};
+  $scope.SignIn = function(e){ 
+     e.preventDefault();
+     var username = $scope.user.email;
+     var password = $scope.user.password;
+     loginObj.$login('password', {
+                email: username,
+                password: password
+            })
+            .then(function(user) {
+                //Success callback
+                console.log('Authentication successful');
+            }, function(error) {
+                //Failure callback
+                console.log('Authentication failure');
+            });
+  }
+}]);
